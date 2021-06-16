df3170f5a87882e84cd7053c87b622bdf61e3003
Moses <lazaromoses77@gmail.com>
2021-06-17 00:13:51 +0300

    adding more about my self

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ function About() {
                 <h4>About Us</h4>
             </div>
             <div className="container_b" data-aos="zoom-out">
-                    <h4>Moses and ScottCate</h4>
-                    <p>Moses and ScottCate are friend for a long time now, They found each other in the most coolest part of the world (a free standing mountain in the world) Kilimanajaro Mountain in Tanzania</p>
+                <h4>I'M MOSES LAZARO</h4>
+                <p>Hey,Welcome to my Website, My Name is Moses Lazaro .I have been learning web development since 2018 as a self tought Developer, I have covered quit a lots of Frontend (Client Side) with HTML CSS AND JAVASCRIPT AND FIGMA for web designs and development. Im also able to do the backend(Server side) with C-sharp programming language and Unity for game development</p>
             </div>
        </section>
     )
